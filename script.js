169baf333a4c70ef72bfeb868030da8ac00c0756
Mithleshkumar5360 <117704988+Mithleshkumar5360@users.noreply.github.com>
2023-04-30 14:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	var table =document.getElementById("SampleTable");
+
+	var newRow = table.insertRow(0);
+
+	var leftCell = newRow.insertCell(0);
+	var rightCell= newRow.insertCell(1);
+
+	leftCell.innerHtml = "New Cell1";
+	rightCell.innerHtml = "New Cell2";
 }
